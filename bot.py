4e75ea0118c20939e98a05275894322dd7f441a5
D4C4N <dacan1508@gmail.com>
2024-02-11 14:08:07 +0100

    Added final version for the bot.

--- bot.py
+++ bot.py
@@ -17,6 +17,7 @@ SERVER_ID = int(os.getenv('SERVER_ID'))
 
 # Some required setup to make this work
 intents = nextcord.Intents.default()
+intents.message_content = True
 client = commands.Bot(command_prefix="!", intents=intents)
 
 # Establishing database connection
@@ -32,7 +33,9 @@ cursor = database.cursor(dictionary=True)
 # Console output upon successful bot launch
 @client.event
 async def on_ready():
-  print("The bot is now deployed and ready for use.")
+  print("##############################################")
+  print("# The bot is now deployed and ready for use. #")
+  print("##############################################")
 
 # Message on member join, using a joke API
 @client.event
@@ -58,45 +61,55 @@ async def on_member_remove(member):
   await channel.send(f"{member.mention} has left us, we are sad to see them go.")
 
 # Slash commands
-@client.slash_command(guild_ids=[SERVER_ID])
+@client.slash_command(guild_ids=[SERVER_ID], description="Our bot will greet you because he's nice :)")
 async def hello(interaction: Interaction):
-  await interaction.response.send_message("Hello, I am a bot.")
+  member = interaction.user.mention
+  await interaction.response.send_message(f"Hello, {member}! I am a bot.")
 
-@client.slash_command(guild_ids=[SERVER_ID])
+@client.slash_command(guild_ids=[SERVER_ID], description="Our bot will be sad because you leave him :(")
 async def bye(interaction: Interaction):
-  await interaction.response.send_message("Why are you leaving me? :(")
+  member = interaction.user.mention
+  await interaction.response.send_message(f"Why are you leaving me, {member}? :(")
 
-@client.slash_command(guild_ids=[SERVER_ID])
+@client.slash_command(guild_ids=[SERVER_ID], description="Do you need help? You shall receive it.")
 async def info(interaction: Interaction):
   await interaction.response.send_message('''
-1. Use ".todo " to write your tasks.
-2. Use ".due (Task) (Due)" to add a due to the task.
-3. Use ".list" to return all tasks.
-4. Use ".removedue (task)" to remove the due from the task.
-5. Use ".remove (task)" to delete a specifc task.
-6. Use ".removeall" to clear the entire list.
+1. Use "/todo" to add a new item to your ToDo-list.
+2. Use "/list" to list all items in your ToDo-list.
+3. Use "/due" to add a due-date to an existing item in your ToDo-list.
+4. Use "/removedue" to remove a due date from a specific item.
+5. Use "/remove" to remove an item from your ToDo-list.
+6. Use "/removeall" to clear your ToDo-list.
                  ''')
 
 # Adds tasks to a database bound to the user's ID
 # splitting is used to prevent the command to be also entered in the database
-@client.slash_command(guild_ids=[SERVER_ID])
+@client.slash_command(guild_ids=[SERVER_ID], description="Use this command to add a new item to your ToDo-List.")
 async def todo(ctx, *, todo_item: str):
-  # Extract the todo item from the command
+  # Extract the todo item from the commandclear
   messageSplit = todo_item
 
-  # Insert the todo item into the database
-  sql = "INSERT INTO todolist (todo, user_id) VALUES (%s, %s)"
-  value = (messageSplit, ctx.author.id)
+  # Check if the todo item already exists for the user
+  sql_check = "SELECT todo FROM todolist WHERE user_id = %s AND todo = %s"
+  value_check = (ctx.user.id, messageSplit)
+  cursor.execute(sql_check, value_check)
+  existing_item = cursor.fetchone()
 
-  cursor.execute(sql, value)
-  database.commit()
+  if existing_item:
+    await ctx.send("This item is already in the list.")
+  else:
+    # Insert the todo item into the database
+    sql_insert = "INSERT INTO todolist (todo, user_id) VALUES (%s, %s)"
+    value_insert = (messageSplit, ctx.user.id)
+    cursor.execute(sql_insert, value_insert)
+    database.commit()
 
-  await ctx.send("Todo item added successfully!")
+    await ctx.send(f"Added {messageSplit} to your ToDo-List!")
 
 # Due to
-@client.slash_command(name="due", description="Set due date for a todo item")
+@client.slash_command(name="due", description="Use this command to set a due date for an existing item in your ToDo-List.")
 async def due(ctx, *, task_and_due_to: str):
-  author_id = ctx.author.id
+  author_id = ctx.user.id
 
   message = task_and_due_to
   taskSplit = ' '.join(message.split()[:-1])
@@ -111,9 +124,9 @@ async def due(ctx, *, task_and_due_to: str):
   await ctx.send("Due date set successfully.")
 
 # List all of the todos from the database bound to ID
-@client.slash_command(name="list", description="List all todos")
+@client.slash_command(name="list", description="Use this command to list all of the items in your ToDo-List.")
 async def list(ctx):
-  author_id = ctx.author.id
+  author_id = ctx.user.id
 
   sql = "SELECT todo, due_to from todolist where user_id = %s"
   value = (author_id,)
@@ -121,19 +134,26 @@ async def list(ctx):
   cursor.execute(sql, value)
 
   rows = cursor.fetchall()
-  for row in rows:
-    value_todo = row["todo"]
-    value_due_to = row["due_to"]
+  
+  if not rows:
+    await ctx.send("Congratulations, there are no items in your list!")
+  else:
+    todos_message = "Your todos:\n"
+    for row in rows:
+      value_todo = row["todo"]
+      value_due_to = row["due_to"]
 
-    if(value_due_to != ""):
-      await ctx.send(f'* "{value_todo}" is due to "{value_due_to}"')
-    else:
-      await ctx.send(f'* {value_todo}')
+      if value_due_to != "":
+        todos_message += f'* "{value_todo}" is due to "{value_due_to}"\n'
+      else:
+        todos_message += f'* {value_todo}\n'
+
+    await ctx.send(todos_message)
 
 # Clear todo list bound to ID
-@client.slash_command(name="removeall", description="Clear todo list")
+@client.slash_command(name="removeall", description="Use this command to clear your ToDo-List.")
 async def removeall(ctx):
-  author_id = ctx.author.id
+  author_id = ctx.user.id
 
   sql = "DELETE FROM todolist WHERE user_id = %s"
   value = (author_id,)
@@ -142,10 +162,10 @@ async def removeall(ctx):
 
   database.commit()
 
-  await ctx.send("Cleared the List.")
+  await ctx.send("All your ToDos have been removed from the list.")
 
 # Remove only one from todo list
-@client.slash_command(name="remove", description="Remove a todo item")
+@client.slash_command(name="remove", description="Use this command to remove a specific entry from your ToDo-List.")
 async def remove(ctx, *, todo_item: str):
   toDeleteSplit = todo_item
 
@@ -159,7 +179,7 @@ async def remove(ctx, *, todo_item: str):
   await ctx.send(f'Removed "{toDeleteSplit}" from the list.')   
 
 # Remove Due
-@client.slash_command(name="removedue", description="Remove due date from a todo item")
+@client.slash_command(name="removedue", description="Use this command to remove a due date for a specific item in your ToDo-List.")
 async def removedue(ctx, *, todo_item: str):
   toDeleteDueSplit = todo_item
 
